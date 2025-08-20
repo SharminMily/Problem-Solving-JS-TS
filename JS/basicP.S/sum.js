5755379e020ca47563326984e95c94b76129a328
SharminMily <sharminakther5599@gmail.com>
2025-08-20 22:00:11 +0600

    4 type of sum

--- JS/basicP.S/sum.js
+++ JS/basicP.S/sum.js
@@ -1,6 +1,14 @@
-// . Sum of First n Natural Numbers
+// 1. Sum of Two Numbers
+// Input: 5, 10 → Output: 30
 
+function sumTwoNumber(a,b){
+  return a + b
+}
+console.log((sumTwoNumber(5, 25)))
+
+// 2. Sum of First n Natural Numbers
 //  Input: n = 5 → Output: 15 (1+2+3+4+5) = // 15
+
 function sumNatural(n){
   let sum = 0;
   for (let i = 1; i <=n; i++){
@@ -13,17 +21,27 @@ function sumNatural(n){
 console.log(sumNatural(5)); 
 
 
+// 3. Sum of All Numbers in an Array
+//  Input: [1, 2, 3, 4, 5] → Output: 15
 
-// Sum of Array Elements
+function sumArray(arr) {
+  let sum = 0;
+  for (let num of arr) {
+    sum += num;
+  }
+  return sum;
+}
+
+console.log(sumArray([1, 2, 3, 4, 5]));
+
+// 4. Sum of Array Elements
+// Input: [1, 2, 3, 4] → Output: 10
 function sumArray(arr) {
   return arr.reduce((sum, num) => sum + num, 0);
 }
 console.log(sumArray([1, 2, 3, 4]));
 
-// Input: [1, 2, 3, 4] → Output: 10
-function sumArrayy(er){
-  return arr.sum
-}
+
 
 
 
