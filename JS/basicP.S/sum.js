f6ab8cf96848ab9c1c40215b733841270da5848e
SharminMily <sharminakther5599@gmail.com>
2025-08-20 21:50:21 +0600

    sum natural

--- JS/basicP.S/sum.js
+++ JS/basicP.S/sum.js
@@ -1,8 +1,30 @@
-// Sum of Array Elements
+// . Sum of First n Natural Numbers
 
+//  Input: n = 5 → Output: 15 (1+2+3+4+5) = // 15
+function sumNatural(n){
+  let sum = 0;
+  for (let i = 1; i <=n; i++){
+
+    sum +=i ;
+  }
+  return sum;
+  }
+
+console.log(sumNatural(5)); 
+
+
+
+// Sum of Array Elements
 function sumArray(arr) {
   return arr.reduce((sum, num) => sum + num, 0);
 }
 console.log(sumArray([1, 2, 3, 4]));
 
 // Input: [1, 2, 3, 4] → Output: 10
+function sumArrayy(er){
+  return arr.sum
+}
+
+
+
+
